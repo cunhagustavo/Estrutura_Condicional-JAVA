d55e2b6137a0ea76290f049634b12f7d87666838
Gustavo Rocha Cunha <rocha.gustavocunha@gmail.com>
2025-04-24 16:10:54 -0300

    Update Exercicio5.java

--- Estrutura_Condicional/Exercicio5.java
+++ Estrutura_Condicional/Exercicio5.java
@@ -1,6 +1,7 @@
 //Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item.
 //A seguir, calcule e mostre o valor da conta a pagar.
 
+
 package Estrutura_Condicional;
 
 import java.util.Locale;
