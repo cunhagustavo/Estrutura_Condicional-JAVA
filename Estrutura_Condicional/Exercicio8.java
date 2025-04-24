f6c84717e166087630d3216f664a9fe40d58448c
Gustavo Rocha Cunha <rocha.gustavocunha@gmail.com>
2025-04-24 16:12:13 -0300

    Update Exercicio8.java

--- Estrutura_Condicional/Exercicio8.java
+++ Estrutura_Condicional/Exercicio8.java
@@ -1,9 +1,6 @@
-//Em um país imaginário denominado Lisarb, todos os habitantes ficam felizes em pagar seus impostos, pois sabem 
-//que nele não existem políticos corruptos e os recursos arrecadados são utilizados em benefício da população, sem 
-//qualquer desvio. A moeda deste país é o Rombus, cujo símbolo é o R$.
+//Em um país imaginário denominado Lisarb, todos os habitantes ficam felizes em pagar seus impostos, pois sabem que nele não existem políticos corruptos e os recursos arrecadados são utilizados em benefício da população, sem qualquer desvio. A moeda deste país é o Rombus, cujo símbolo é o R$.
 
-//Leia um valor com duas casas decimais, equivalente ao salário de uma pessoa de Lisarb. Em seguida, calcule e 
-//mostre o valor que esta pessoa deve pagar de Imposto de Renda, segundo a tabela abaixo.
+//Leia um valor com duas casas decimais, equivalente ao salário de uma pessoa de Lisarb. Em seguida, calcule e mostre o valor que esta pessoa deve pagar de Imposto de Renda.
 
 //Lembre que, se o salário for R$ 3002.00, a taxa que incide é de 8% apenas sobre R$ 1000.00, pois a faixa de 
 //salário que fica de R$ 0.00 até R$ 2000.00 é isenta de Imposto de Renda. No exemplo fornecido (abaixo), a taxa é 
